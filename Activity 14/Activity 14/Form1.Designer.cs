dcd221f63cee565b67d57be27f22340c5e03a52f
ChazPollino <132384915+ChazPollino@users.noreply.github.com>
2023-06-12 16:15:09 -0400

    Add files via upload

---
+++ Activity 14/Activity 14/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Activity_14
+{
+    partial class Activity14
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.KarenRadioButton = new System.Windows.Forms.RadioButton();
+            this.BobRadioButton = new System.Windows.Forms.RadioButton();
+            this.AdalineRadioButton = new System.Windows.Forms.RadioButton();
+            this.StartTimeCheckBox1 = new System.Windows.Forms.CheckBox();
+            this.StartTimeCheckBox2 = new System.Windows.Forms.CheckBox();
+            this.EndCheckBox1 = new System.Windows.Forms.CheckBox();
+            this.Namelabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Submit2Button = new System.Windows.Forms.Button();
+            this.StartTimeCheckBox3 = new System.Windows.Forms.CheckBox();
+            this.EndCheckBox2 = new System.Windows.Forms.CheckBox();
+            this.WorkCheckinbutton = new System.Windows.Forms.Button();
+            this.CheckinLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.Startlabel = new System.Windows.Forms.Label();
+            this.EndLabel = new System.Windows.Forms.Label();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Items.AddRange(new object[] {
+            "Employee",
+            "Manager"});
+            this.listBox1.Location = new System.Drawing.Point(470, 186);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 66);
+            this.listBox1.TabIndex = 1;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.SubmitButton.Location = new System.Drawing.Point(553, 373);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(238, 59);
+            this.SubmitButton.TabIndex = 7;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // KarenRadioButton
+            // 
+            this.KarenRadioButton.AutoSize = true;
+            this.KarenRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.KarenRadioButton.Location = new System.Drawing.Point(25, 360);
+            this.KarenRadioButton.Name = "KarenRadioButton";
+            this.KarenRadioButton.Size = new System.Drawing.Size(117, 35);
+            this.KarenRadioButton.TabIndex = 8;
+            this.KarenRadioButton.TabStop = true;
+            this.KarenRadioButton.Text = "Karen";
+            this.KarenRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // BobRadioButton
+            // 
+            this.BobRadioButton.AutoSize = true;
+            this.BobRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.BobRadioButton.Location = new System.Drawing.Point(25, 325);
+            this.BobRadioButton.Name = "BobRadioButton";
+            this.BobRadioButton.Size = new System.Drawing.Size(93, 35);
+            this.BobRadioButton.TabIndex = 9;
+            this.BobRadioButton.TabStop = true;
+            this.BobRadioButton.Text = "Bob";
+            this.BobRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // AdalineRadioButton
+            // 
+            this.AdalineRadioButton.AutoSize = true;
+            this.AdalineRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.AdalineRadioButton.Location = new System.Drawing.Point(25, 290);
+            this.AdalineRadioButton.Name = "AdalineRadioButton";
+            this.AdalineRadioButton.Size = new System.Drawing.Size(135, 35);
+            this.AdalineRadioButton.TabIndex = 10;
+            this.AdalineRadioButton.TabStop = true;
+            this.AdalineRadioButton.Text = "Adaline";
+            this.AdalineRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // StartTimeCheckBox1
+            // 
+            this.StartTimeCheckBox1.AutoSize = true;
+            this.StartTimeCheckBox1.Location = new System.Drawing.Point(45, 596);
+            this.StartTimeCheckBox1.Name = "StartTimeCheckBox1";
+            this.StartTimeCheckBox1.Size = new System.Drawing.Size(124, 29);
+            this.StartTimeCheckBox1.TabIndex = 11;
+            this.StartTimeCheckBox1.Text = "6:00 AM";
+            this.StartTimeCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // StartTimeCheckBox2
+            // 
+            this.StartTimeCheckBox2.AutoSize = true;
+            this.StartTimeCheckBox2.Location = new System.Drawing.Point(45, 671);
+            this.StartTimeCheckBox2.Name = "StartTimeCheckBox2";
+            this.StartTimeCheckBox2.Size = new System.Drawing.Size(124, 29);
+            this.StartTimeCheckBox2.TabIndex = 12;
+            this.StartTimeCheckBox2.Text = "7:00 AM";
+            this.StartTimeCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // EndCheckBox1
+            // 
+            this.EndCheckBox1.AutoSize = true;
+            this.EndCheckBox1.Location = new System.Drawing.Point(232, 692);
+            this.EndCheckBox1.Name = "EndCheckBox1";
+            this.EndCheckBox1.Size = new System.Drawing.Size(136, 29);
+            this.EndCheckBox1.TabIndex = 13;
+            this.EndCheckBox1.Text = "11:00 PM";
+            this.EndCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // Namelabel
+            // 
+            this.Namelabel.BackColor = System.Drawing.Color.White;
+            this.Namelabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Namelabel.Location = new System.Drawing.Point(503, 278);
+            this.Namelabel.Name = "Namelabel";
+            this.Namelabel.Size = new System.Drawing.Size(326, 82);
+            this.Namelabel.TabIndex = 14;
+            this.Namelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Namelabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(503, 577);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 67);
+            this.label2.TabIndex = 15;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Submit2Button
+            // 
+            this.Submit2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Submit2Button.Location = new System.Drawing.Point(571, 662);
+            this.Submit2Button.Name = "Submit2Button";
+            this.Submit2Button.Size = new System.Drawing.Size(238, 59);
+            this.Submit2Button.TabIndex = 16;
+            this.Submit2Button.Text = "Submit";
+            this.Submit2Button.UseVisualStyleBackColor = true;
+            this.Submit2Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StartTimeCheckBox3
+            // 
+            this.StartTimeCheckBox3.AutoSize = true;
+            this.StartTimeCheckBox3.Location = new System.Drawing.Point(45, 742);
+            this.StartTimeCheckBox3.Name = "StartTimeCheckBox3";
+            this.StartTimeCheckBox3.Size = new System.Drawing.Size(124, 29);
+            this.StartTimeCheckBox3.TabIndex = 18;
+            this.StartTimeCheckBox3.Text = "8:00 AM";
+            this.StartTimeCheckBox3.UseVisualStyleBackColor = true;
+            // 
+            // EndCheckBox2
+            // 
+            this.EndCheckBox2.AutoSize = true;
+            this.EndCheckBox2.Location = new System.Drawing.Point(232, 615);
+            this.EndCheckBox2.Name = "EndCheckBox2";
+            this.EndCheckBox2.Size = new System.Drawing.Size(136, 29);
+            this.EndCheckBox2.TabIndex = 19;
+            this.EndCheckBox2.Text = "10:00 PM";
+            this.EndCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // WorkCheckinbutton
+            // 
+            this.WorkCheckinbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.WorkCheckinbutton.Location = new System.Drawing.Point(232, 812);
+            this.WorkCheckinbutton.Name = "WorkCheckinbutton";
+            this.WorkCheckinbutton.Size = new System.Drawing.Size(351, 75);
+            this.WorkCheckinbutton.TabIndex = 20;
+            this.WorkCheckinbutton.Text = "Check Into Work";
+            this.WorkCheckinbutton.UseVisualStyleBackColor = true;
+            this.WorkCheckinbutton.Click += new System.EventHandler(this.WorkCheckinbutton_Click);
+            // 
+            // CheckinLabel
+            // 
+            this.CheckinLabel.BackColor = System.Drawing.Color.White;
+            this.CheckinLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CheckinLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.CheckinLabel.Location = new System.Drawing.Point(788, 778);
+            this.CheckinLabel.Name = "CheckinLabel";
+            this.CheckinLabel.Size = new System.Drawing.Size(563, 137);
+            this.CheckinLabel.TabIndex = 21;
+            this.CheckinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TitleLabel.Location = new System.Drawing.Point(242, 21);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(832, 83);
+            this.TitleLabel.TabIndex = 22;
+            this.TitleLabel.Text = "Work Check In";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Startlabel
+            // 
+            this.Startlabel.BackColor = System.Drawing.Color.White;
+            this.Startlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Startlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Startlabel.Location = new System.Drawing.Point(25, 534);
+            this.Startlabel.Name = "Startlabel";
+            this.Startlabel.Size = new System.Drawing.Size(167, 40);
+            this.Startlabel.TabIndex = 23;
+            this.Startlabel.Text = "Start Time";
+            this.Startlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EndLabel
+            // 
+            this.EndLabel.BackColor = System.Drawing.Color.White;
+            this.EndLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EndLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.EndLabel.Location = new System.Drawing.Point(232, 534);
+            this.EndLabel.Name = "EndLabel";
+            this.EndLabel.Size = new System.Drawing.Size(160, 40);
+            this.EndLabel.TabIndex = 24;
+            this.EndLabel.Text = "End Time";
+            this.EndLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Clearbutton.Location = new System.Drawing.Point(1012, 414);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(245, 85);
+            this.Clearbutton.TabIndex = 26;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Activity14
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1381, 937);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.EndLabel);
+            this.Controls.Add(this.Startlabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.CheckinLabel);
+            this.Controls.Add(this.WorkCheckinbutton);
+            this.Controls.Add(this.EndCheckBox2);
+            this.Controls.Add(this.StartTimeCheckBox3);
+            this.Controls.Add(this.Submit2Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Namelabel);
+            this.Controls.Add(this.EndCheckBox1);
+            this.Controls.Add(this.StartTimeCheckBox2);
+            this.Controls.Add(this.StartTimeCheckBox1);
+            this.Controls.Add(this.AdalineRadioButton);
+            this.Controls.Add(this.BobRadioButton);
+            this.Controls.Add(this.KarenRadioButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Activity14";
+            this.Text = "Work Check-In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.RadioButton KarenRadioButton;
+        private System.Windows.Forms.RadioButton BobRadioButton;
+        private System.Windows.Forms.RadioButton AdalineRadioButton;
+        private System.Windows.Forms.CheckBox StartTimeCheckBox1;
+        private System.Windows.Forms.CheckBox StartTimeCheckBox2;
+        private System.Windows.Forms.CheckBox EndCheckBox1;
+        private System.Windows.Forms.Label Namelabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Submit2Button;
+        private System.Windows.Forms.CheckBox StartTimeCheckBox3;
+        private System.Windows.Forms.CheckBox EndCheckBox2;
+        private System.Windows.Forms.Button WorkCheckinbutton;
+        private System.Windows.Forms.Label CheckinLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label Startlabel;
+        private System.Windows.Forms.Label EndLabel;
+        private System.Windows.Forms.Button Clearbutton;
+    }
+}
+
---
+++ Activity 14/Activity 14/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_14
+{
+    public partial class Activity14 : Form
+    {
+        public Activity14()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmployeeListBox_DoubleClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void AdoptButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+
+            if (AdalineRadioButton.Checked) 
+            {
+                Namelabel.Text = "Adaline";
+            }
+            if (BobRadioButton.Checked)
+            {
+                Namelabel.Text = "Bob";
+            }
+            if (KarenRadioButton.Checked)
+            {
+                Namelabel.Text = "Karen";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (StartTimeCheckBox1.Checked ^ EndCheckBox1.Checked)
+            {
+                label2.Text = Namelabel.Text + "    6:00 AM TILL 10:00 PM";
+            }
+            if (StartTimeCheckBox1.Checked ^ EndCheckBox2.Checked)
+            {
+                label2.Text = Namelabel.Text + "    6:00 AM TILL 11:00 PM";
+            }
+            if (StartTimeCheckBox2.Checked ^ EndCheckBox1.Checked)
+            {
+                label2.Text = Namelabel.Text + "    7:00 AM TILL 10:00 PM";
+            }
+            if (StartTimeCheckBox2.Checked ^ EndCheckBox2.Checked)
+            {
+                label2.Text = Namelabel.Text + "    7:00 AM TILL 11:00 PM";
+            }
+            if (StartTimeCheckBox3.Checked ^ EndCheckBox1.Checked)
+            {
+                label2.Text = Namelabel.Text + "    8:00 AM TILL 10:00 PM";
+            }
+            if (StartTimeCheckBox3.Checked ^ EndCheckBox2.Checked)
+            {
+                label2.Text = Namelabel.Text + "    8:00 AM TILL 11:00 PM";
+            }
+            StartTimeCheckBox1.Checked = false;
+            StartTimeCheckBox2.Checked = false;
+            StartTimeCheckBox3.Checked = false;
+            EndCheckBox1.Checked = false;
+            EndCheckBox2.Checked = false;
+        }
+
+        private void WorkCheckinbutton_Click(object sender, EventArgs e)
+        {
+            
+            CheckinLabel.Text =  "You have successfully checked in";
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            Namelabel.Text = "";
+            label2.Text = "";
+            CheckinLabel.Text = "";
+        }
+    }
+}
